3419aa4a7d8c70cd5e59d44f143e2e4bec065f7e
fabriziodisanto <32740687+fabriziodisanto@users.noreply.github.com>
2025-03-14 13:45:25 -0300

    Update video-con-trackbar.py

--- tutorial/video-con-trackbar.py
+++ tutorial/video-con-trackbar.py
@@ -1,57 +1,53 @@
 import cv2
 
-
 def create_trackbar(trackbar_name, window_name, slider_max):
-    cv2.createTrackbar(trackbar_name, window_name, 0, slider_max, on_trackbar)
-
+    cv2.createTrackbar(trackbar_name, window_name, 1, slider_max, on_trackbar)
 
 def on_trackbar(val):
     pass
-
 
 def get_trackbar_value(trackbar_name, window_name):
     return int(cv2.getTrackbarPos(trackbar_name, window_name))
 
 
 WINDOW_NAME = 'WINDOW'
-COLOR_RED = (0, 0, 255)
 
-TRACKBAR_X_NAME = 'X length'
-TRACKBAR_X_SLIDER_MAX = 1200
+TRACKBAR_THRESH_NAME = 'Threshold'
+TRACKBAR_THRESH_SLIDER_MAX = 255
 
-TRACKBAR_Y_NAME = 'Y length'
-TRACKBAR_Y_SLIDER_MAX = 900
-
-RECTANGLE_STARTING_POINT = (50, 50)
+TRACKBAR_KERNEL_NAME = 'Kernel size'
+TRACKBAR_KERNEL_SLIDER_MAX = 10
 
 
 def main():
     cv2.namedWindow(WINDOW_NAME)
-    create_trackbar(TRACKBAR_X_NAME, WINDOW_NAME, TRACKBAR_X_SLIDER_MAX)
-    create_trackbar(TRACKBAR_Y_NAME, WINDOW_NAME, TRACKBAR_Y_SLIDER_MAX)
+    create_trackbar(TRACKBAR_THRESH_NAME, WINDOW_NAME, TRACKBAR_THRESH_SLIDER_MAX)
+    create_trackbar(TRACKBAR_KERNEL_NAME, WINDOW_NAME, TRACKBAR_KERNEL_SLIDER_MAX)
     cap = cv2.VideoCapture(0)
 
     while True:
-        ret, frame = cap.read()
+        _, frame = cap.read()
+        cv2.imshow(WINDOW_NAME, frame)
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('gray', gray)
 
-        trackbar_x_value = get_trackbar_value(TRACKBAR_X_NAME, WINDOW_NAME)
-        trackbar_y_value = get_trackbar_value(TRACKBAR_Y_NAME, WINDOW_NAME)
+        trackbar_thresh_value = get_trackbar_value(TRACKBAR_THRESH_NAME, WINDOW_NAME)
+        _, thresh = cv2.threshold(gray, trackbar_thresh_value, 255, cv2.THRESH_BINARY)
+        cv2.imshow('binary', thresh)
 
-        rectangle_end_point = (RECTANGLE_STARTING_POINT[0] + trackbar_x_value,
-                               RECTANGLE_STARTING_POINT[1] + trackbar_y_value)
+        kernel_size_value = get_trackbar_value(TRACKBAR_KERNEL_NAME, WINDOW_NAME)
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size_value, kernel_size_value))
+        
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        # cv2.imshow('opening', opening)
 
-        cv2.rectangle(frame,
-                      RECTANGLE_STARTING_POINT,
-                      rectangle_end_point,
-                      COLOR_RED,
-                      2)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+        cv2.imshow('closing', closing)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             break
 
-        cv2.imshow(WINDOW_NAME, frame)
-
-    cap.release()
     cv2.destroyAllWindows()
 
 
